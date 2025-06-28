62e0fa531f8b49da88056f3c63840347a7d91268
Sarah <109141163+AutumnsCode@users.noreply.github.com>
2025-06-28 19:32:59 +0200

    implement cta

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,4 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const CTA = () => {
-  return <div>Cta</div>;
+  return (
+    <section className="cta-section">
+      <div className="cta-badge">Start learning your way.</div>
+      <h2 className="text-3xl font-bold">
+        Build and Personalize Learning Compasion
+      </h2>
+      <p>
+        Pick a name, subject, voice, & personality — and start learning through
+        voice conversations that feel natural and fun.
+      </p>
+      <Image src="/images/cta.svg" alt="cta" width={362} height={232} />
+      <Link href="/companions/new">
+        <p>Build a New Companion</p>
+      </Link>
+    </section>
+  );
 };
 export default CTA;
